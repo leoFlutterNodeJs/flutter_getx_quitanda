a71f8d9c9efe2e4533ebf406f8eb4626aee96203
leoFlutterNodeJs <leodevnew@gmail.com>
2022-07-18 08:37:39 -0300

    feat: add custom text field to login screen

---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import './components/custom_text_field.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Column(
+        children: [
+          Expanded(
+              child: Container(
+            color: Colors.green,
+          )),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(45),
+                  )),
+              child: Column(
+                children: const [
+                  CustomTextField(
+                    icon: Icons.email,
+                    label: 'E-mail',
+                  ),
+                  CustomTextField(
+                    icon: Icons.lock,
+                    label: 'Senha',
+                    isSecret: true,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
